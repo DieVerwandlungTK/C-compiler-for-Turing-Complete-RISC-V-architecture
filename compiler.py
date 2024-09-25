cf2fe01aa14de9c99be468a9e5cda91c534f120d
Takato Kinoshita <sd22039@oiwanoMac-mini.local>
2024-09-25 20:25:39 +0900

    update compiler

--- compiler.py
+++ compiler.py
@@ -1,7 +1,8 @@
 import sys
 from tokenizer import Tokenizer, TokenType
+from syntax_tree import Parser, Node, NodeType
 
-def strtol(s):
+def strtol(s: str) -> tuple[int, str]:
     n = 0
     cnt = 0
     for i, c in enumerate(s):
@@ -13,38 +14,89 @@ def strtol(s):
     s = s[cnt + 1:]
     return n, s
 
+class Compiler():
+    def __init__(self, file_path: str) -> None:
+        self.file_name = file_path
+    
+    def compile(self, node: Node) -> None:
+        f = open(self.file_name, "w")
+        f.close()
+
+        f = open(self.file_name, "a")
+        f.write("main:\n")
+        def _recursive_compile(node: Node) -> None:
+            if node.node_type == NodeType.ND_ADD:
+                _recursive_compile(node.rhs)
+                _recursive_compile(node.lhs)
+                f.write("   lw t0, 0(sp)\n")
+                f.write("   li t2, 4\n")
+                f.write("   sub sp, sp, t2\n")
+                f.write("   lw t1, 0(sp)\n")
+                f.write("   sub sp, sp, t2\n")
+                f.write("   add t0, t0, t1\n")
+                f.write("   sw t0, 0(sp)\n")
+                f.write("   addi sp, sp, 4\n")
+
+            elif node.node_type == NodeType.ND_SUB:
+                _recursive_compile(node.rhs)
+                _recursive_compile(node.lhs)
+                f.write("   lw t0, 0(sp)\n")
+                f.write("   li t2, 4\n")
+                f.write("   sub sp, sp, t2\n")
+                f.write("   lw t1, 0(sp)\n")
+                f.write("   sub sp, sp, t2\n")
+                f.write("   sub t0, t0, t1\n")
+                f.write("   sw t0, 0(sp)\n")
+                f.write("   addi sp, sp, 4\n")
+            
+            elif node.node_type == NodeType.ND_MUL:
+                _recursive_compile(node.rhs)
+                _recursive_compile(node.lhs)
+                f.write("   lw t0, 0(sp)\n")
+                f.write("   li t2, 4\n")
+                f.write("   sub sp, sp, t2\n")
+                f.write("   lw t1, 0(sp)\n")
+                f.write("   sub sp, sp, t2\n")
+                # hoge
+            
+            elif node.node_type == NodeType.ND_DIV:
+                _recursive_compile(node.rhs)
+                _recursive_compile(node.lhs)
+                f.write("   lw t0, 0(sp)\n")
+                f.write("   li t2, 4\n")
+                f.write("   sub sp, sp, t2\n")
+                f.write("   lw t1, 0(sp)\n")
+                f.write("   sub sp, sp, t2\n")
+                # fuga
+            
+            elif node.node_type == NodeType.ND_NUM:
+                f.write(f"   li t0, {node.val}\n")
+                f.write("   sw t0, 0(sp)\n")
+                f.write("   li t0, 4\n")
+                f.write("   addi sp, sp, 4\n")
+            
+            else:
+                sys.exit(1)
+        
+        _recursive_compile(node)
+        f.write("   ret\n")
+        f.close()
+
+                
+
+
+
+
 if __name__ == "__main__":
     args = sys.argv
     if len(args) != 3:
         print("The given number of arguments is invalid.", file=sys.stderr)
         sys.exit(1)
 
-    with open(args[2], "w") as f:
-        f.write("main:\n")
-
-        tokenizer = Tokenizer()
-        tokenizer.tokenize(open(args[1], "r").read())
-
-        while not tokenizer.at_eof():
-            if tokenizer.consume("+"):
-                f.write(" addi ")
-                f.write(f"a0, a0, {tokenizer.expect_number()}\n")
-                continue
-
-            if tokenizer.consume("-"):
-                f.write(" li ")
-                f.write(f"t0, {tokenizer.expect_number()}\n")
-                f.write(" sub ")
-                f.write(f"a0, a0, t0\n")
-                continue
-
-            if tokenizer.tokens[0].type == TokenType.TK_NUM:
-                f.write(" li ")
-                f.write(f"a0, {tokenizer.expect_number()}\n")
-                continue
-
-            print(f"Unexpected token: {tokenizer.tokens[0].token_str}", file=sys.stderr)
-            sys.exit(1)
-        
-                
-        f.write(" ret\n")
+    
+    tokenizer = Tokenizer()
+    tokenizer.tokenize(open(args[1], "r").read())
+    parser = Parser(tokenizer)
+    parser.parse()
+    compiler = Compiler(args[2])
+    compiler.compile(parser.root)
