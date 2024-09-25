617b2836ac2f8236b66e3923f4415ca1c4daee56
DieVerwandlungTK <buri0901uni0701@icloud.com>
2024-09-25 19:22:15 +0900

    update parser

--- parser.py
+++ parser.py
@@ -17,4 +17,43 @@ class Node():
 
 class Parser():
     def __init__(self, tokenizer: Tokenizer) -> None:
-        self.tokenizer = tokenizer
+        self.tokenizer: Tokenizer = tokenizer
+        self.root: Node | None = None
+    
+    def _expr(self) -> Node:
+        node = self._mul()
+
+        while True:
+            if self.tokenizer.consume("+"):
+                node = Node(NodeType.ND_ADD, node, self._mul())
+            elif self.tokenizer.consume("-"):
+                node = Node(NodeType.ND_SUB, node, self._mul())
+            else:
+                break
+
+        return node
+
+    def _mul(self) -> Node:
+        node = self._primary()
+
+        while True:
+            if self.tokenizer.consume("*"):
+                node = Node(NodeType.ND_MUL, node, self._primary())
+            elif self.tokenizer.consume("/"):
+                node = Node(NodeType.ND_DIV, node, self._primary())
+            else:
+                break
+        
+        return node
+    
+    def _primary(self) -> Node:
+        if self.tokenizer.consume("("):
+            node = self._expr()
+            self.tokenizer.expect(")")
+            return node
+        else:
+            return Node(NodeType.ND_NUM, val=self.tokenizer.expect_number())
+    
+    def parse(self) -> None:
+        self.root = self._expr()
+        
