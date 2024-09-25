fb38c627987699e751dd10f106599a7478f1dc8d
Takato Kinoshita <sd22039@oiwanoMac-mini.local>
2024-09-25 20:23:22 +0900

    change file name

---
+++ syntax_tree.py
@@ -0,0 +1,59 @@
+from enum import Enum
+from tokenizer import Tokenizer
+
+class NodeType(Enum):
+    ND_ADD = 0
+    ND_SUB = 1
+    ND_MUL = 2
+    ND_DIV = 3
+    ND_NUM = 4
+
+class Node():
+    def __init__(self, type, lhs = None, rhs = None, val = None) -> None:
+        self.node_type = type
+        self.lhs = lhs
+        self.rhs = rhs
+        self.val = val
+
+class Parser():
+    def __init__(self, tokenizer: Tokenizer) -> None:
+        self.tokenizer: Tokenizer = tokenizer
+        self.root: Node | None = None
+    
+    def _expr(self) -> Node:
+        node = self._mul()
+
+        while True:
+            if self.tokenizer.consume("+"):
+                node = Node(NodeType.ND_ADD, node, self._mul())
+            elif self.tokenizer.consume("-"):
+                node = Node(NodeType.ND_SUB, node, self._mul())
+            else:
+                break
+
+        return node
+
+    def _mul(self) -> Node:
+        node = self._primary()
+
+        while True:
+            if self.tokenizer.consume("*"):
+                node = Node(NodeType.ND_MUL, node, self._primary())
+            elif self.tokenizer.consume("/"):
+                node = Node(NodeType.ND_DIV, node, self._primary())
+            else:
+                break
+        
+        return node
+    
+    def _primary(self) -> Node:
+        if self.tokenizer.consume("("):
+            node = self._expr()
+            self.tokenizer.expect(")")
+            return node
+        else:
+            return Node(NodeType.ND_NUM, val=self.tokenizer.expect_number())
+    
+    def parse(self) -> None:
+        self.root = self._expr()
+        
