f0848448d39f26885b661a5a6f457e8e5c8a9299
Takato Kinoshita <sd22039@oiwanoMac-mini.local>
2024-09-27 14:17:18 +0900

    refactored

--- linker.py
+++ linker.py
@@ -2,11 +2,62 @@ import sys
 from utils import strtol
 
 class Linker():
-    REGISTER_MAP = {"zero": "00000", "ra": "00001", "sp": "00010", "t0": "00101", 
-                    "t1": "00110", "t2": "00111", "a0": "01010", "t3": "11100"}
+    REGISTER_MAP = {"zero": "00000", "ra": "00001", "sp": "00010", "gp": "00011", "tp": "000100", "t0": "00101",
+                    "t1": "00110", "t2": "00111", "fp": "01000", "s0": "01000", "s1": "01001", "a0": "01010",
+                    "a1": "01011", "a2": "01100", "a3": "01101", "a4": "01110", "a5": "01111", "a6": "10000",
+                    "a7": "10001", "s2": "10010", "s3": "10011", "s4": "10100", "s5": "10101", "s6": "10110",
+                    "s7": "10111", "s8": "11000", "s9": "11001", "s10": "11010", "s11": "11011", "t3": "11100",
+                    "t4": "11101", "t5": "11110", "t6": "11111"}
     
     def __init__(self) -> None:
         pass
+    
+    @staticmethod
+    def _r_instruction(rd: str, rs1: str, rs2: str, funct3: str, funct7: str) -> str:
+        return f"{funct7}{Linker.REGISTER_MAP[rs2]}{Linker.REGISTER_MAP[rs1]}{funct3}{Linker.REGISTER_MAP[rd]}0110011"
+    
+    @staticmethod
+    def _i_instruction(opcode: str, rd: str, rs1: str, imm: int, funct3: str) -> str:
+        return f"{imm:012b}{Linker.REGISTER_MAP[rs1]}{funct3}{Linker.REGISTER_MAP[rd]}{opcode}"
+    
+    @staticmethod
+    def _arithmetic_i_instruction(rd: str, rs1: str, rs2: str, funct3: str) -> str:
+        return Linker._i_instruction("0010011", rd, rs1, int(rs2), funct3)
+    
+    @staticmethod
+    def _load_i_instruction(rd: str, rs1: str, imm: int, funct3: str) -> str:
+        return Linker._i_instruction("0000011", rd, rs1, imm, funct3)
+    
+    @staticmethod
+    def _jalr_i_instruction(rd: str, rs1: str, imm: int) -> str:
+        return Linker._i_instruction("1100111", rd, rs1, imm, "000")
+    
+    @staticmethod
+    def _s_instruction(rs2: str, rs1: str, imm: int, funct3: str) -> str:
+        imm = f"{imm:012b}"
+        return f"{imm[:7]}{Linker.REGISTER_MAP[rs2]}{Linker.REGISTER_MAP[rs1]}{funct3}{imm[7:]}0100011"
+    
+    @staticmethod
+    def _b_instruction(rs1: str, rs2: str, imm: int, funct3: str) -> str:
+        imm = f"{imm:012b}"
+        return f"{imm[0]}{imm[2:8]}{Linker.REGISTER_MAP[rs2]}{Linker.REGISTER_MAP[rs1]}{funct3}{imm[8:]}{imm[1]}1100011"
+    
+    @staticmethod
+    def _u_instruction(opcode: str, rd: str, imm: int) -> str:
+        return f"{imm:020b}{Linker.REGISTER_MAP[rd]}{opcode}"
+    
+    @staticmethod
+    def _lui_u_instruction(rd: str, imm: int) -> str:
+        return Linker._u_instruction("0110111", rd, imm)
+    
+    @staticmethod
+    def _auipc_u_instruction(rd: str, imm: int) -> str:
+        return Linker._u_instruction("0010111", rd, imm)
+    
+    @staticmethod
+    def _j_instruction(rd: str, imm: int) -> str:
+        imm = f"{imm:020b}"
+        return f"{imm[0]}{imm[10]}{imm[9]}{imm[1:9]}{Linker.REGISTER_MAP[rd]}1101111"
 
     def link(self, file_path: str) -> None:
         with open("out.bin", "bw") as out:
@@ -25,60 +76,26 @@ class Linker():
                     continue
 
                 elif toks[0] == "li":
-                    bin += f"{int(toks[2]):012b}"
-                    bin += self.REGISTER_MAP["zero"]
-                    bin += "000"
-                    bin += self.REGISTER_MAP[toks[1]]
-                    bin += "00100"
-                    bin += "11"
+                    bin = Linker._arithmetic_i_instruction(toks[1], "zero", toks[2], "000")
                 
                 elif toks[0] == "addi":
-                    bin += f"{int(toks[3]):012b}"
-                    bin += f"{self.REGISTER_MAP[toks[2]]}"
-                    bin += "000"
-                    bin += self.REGISTER_MAP[toks[1]]
-                    bin += "00100"
-                    bin += "11"
+                    bin = Linker._arithmetic_i_instruction(toks[1], toks[2], toks[3], "000")
                 
                 elif toks[0] == "add":
-                    bin += "00000"
-                    bin += "00"
-                    bin += f"{self.REGISTER_MAP[toks[3]]}"
-                    bin += f"{self.REGISTER_MAP[toks[2]]}"
-                    bin += "000"
-                    bin += self.REGISTER_MAP[toks[1]]
-                    bin += "01100"
-                    bin += "11"
+                    bin = Linker._r_instruction(toks[1], toks[2], toks[3], "000", "0000000")
                 
                 elif toks[0] == "sub":
-                    bin += "01000"
-                    bin += "00"
-                    bin += f"{self.REGISTER_MAP[toks[3]]}"
-                    bin += f"{self.REGISTER_MAP[toks[2]]}"
-                    bin += "000"
-                    bin += self.REGISTER_MAP[toks[1]]
-                    bin += "01100"
-                    bin += "11"
+                    bin = Linker._r_instruction(toks[1], toks[2], toks[3], "000", "0100000")
                 
                 elif toks[0] == "lw":
-                    n, rest = strtol(toks[2])
-                    bin += f"{n:012b}"
-                    bin += f"{self.REGISTER_MAP[rest.replace('(', '').replace(')', '')]}"
-                    bin += "010"
-                    bin += self.REGISTER_MAP[toks[1]]
-                    bin += "00000"
-                    bin += "11"
+                    imm, rest = strtol(toks[2])
+                    rs1 = rest.replace("(", "").replace(")", "")
+                    bin = Linker._load_i_instruction(toks[1], rs1, imm, "010")
                 
                 elif toks[0] == "sw":
-                    n, rest = strtol(toks[2])
-                    offset = f"{n:012b}"
-                    bin += offset[:7]
-                    bin += f"{self.REGISTER_MAP[toks[1]]}"
-                    bin += f"{self.REGISTER_MAP[rest.replace('(', '').replace(')', '')]}"
-                    bin += "010"
-                    bin += offset[7:]
-                    bin += "01000"
-                    bin += "11"
+                    imm, rest = strtol(toks[2])
+                    rs1 = rest.replace("(", "").replace(")", "")
+                    bin = Linker._s_instruction(toks[1], rs1, imm, "010")
 
                 elif line == "ret":
                     continue
