0339cd5e3babdfe961f6cd4338fa2f2b45acdc20
DieVerwandlungTK <buri0901uni0701@icloud.com>
2024-09-28 23:11:36 +0900

    renamed

--- assembler.py
+++ assembler.py
@@ -1,7 +1,7 @@
 import sys
 from utils import strtol
 
-class Linker():
+class Assembler():
     REGISTER_MAP = {"zero": "00000", "ra": "00001", "sp": "00010", "gp": "00011", "tp": "000100", "t0": "00101",
                     "t1": "00110", "t2": "00111", "fp": "01000", "s0": "01000", "s1": "01001", "a0": "01010",
                     "a1": "01011", "a2": "01100", "a3": "01101", "a4": "01110", "a5": "01111", "a6": "10000",
@@ -14,52 +14,52 @@ class Linker():
     
     @staticmethod
     def _r_instruction(rd: str, rs1: str, rs2: str, funct3: str, funct7: str) -> str:
-        return f"{funct7}{Linker.REGISTER_MAP[rs2]}{Linker.REGISTER_MAP[rs1]}{funct3}{Linker.REGISTER_MAP[rd]}0110011"
+        return f"{funct7}{Assembler.REGISTER_MAP[rs2]}{Assembler.REGISTER_MAP[rs1]}{funct3}{Assembler.REGISTER_MAP[rd]}0110011"
     
     @staticmethod
     def _i_instruction(opcode: str, rd: str, rs1: str, imm: str, funct3: str) -> str:
-        return f"{int(imm):012b}{Linker.REGISTER_MAP[rs1]}{funct3}{Linker.REGISTER_MAP[rd]}{opcode}"
+        return f"{int(imm):012b}{Assembler.REGISTER_MAP[rs1]}{funct3}{Assembler.REGISTER_MAP[rd]}{opcode}"
     
     @staticmethod
     def _arithmetic_i_instruction(rd: str, rs1: str, imm: str, funct3: str) -> str:
-        return Linker._i_instruction("0010011", rd, rs1, imm, funct3)
+        return Assembler._i_instruction("0010011", rd, rs1, imm, funct3)
     
     @staticmethod
     def _load_i_instruction(rd: str, rs1: str, imm: str, funct3: str) -> str:
-        return Linker._i_instruction("0000011", rd, rs1, imm, funct3)
+        return Assembler._i_instruction("0000011", rd, rs1, imm, funct3)
     
     @staticmethod
     def _jalr_i_instruction(rd: str, rs1: str, imm: str) -> str:
-        return Linker._i_instruction("1100111", rd, rs1, imm, "000")
+        return Assembler._i_instruction("1100111", rd, rs1, imm, "000")
     
     @staticmethod
     def _s_instruction(rs2: str, rs1: str, imm: str, funct3: str) -> str:
         imm = f"{int(imm):012b}"
-        return f"{imm[:7]}{Linker.REGISTER_MAP[rs2]}{Linker.REGISTER_MAP[rs1]}{funct3}{imm[7:]}0100011"
+        return f"{imm[:7]}{Assembler.REGISTER_MAP[rs2]}{Assembler.REGISTER_MAP[rs1]}{funct3}{imm[7:]}0100011"
     
     @staticmethod
     def _b_instruction(rs1: str, rs2: str, imm: str, funct3: str) -> str:
         imm = f"{int(imm):012b}"
-        return f"{imm[0]}{imm[2:8]}{Linker.REGISTER_MAP[rs2]}{Linker.REGISTER_MAP[rs1]}{funct3}{imm[8:]}{imm[1]}1100011"
+        return f"{imm[0]}{imm[2:8]}{Assembler.REGISTER_MAP[rs2]}{Assembler.REGISTER_MAP[rs1]}{funct3}{imm[8:]}{imm[1]}1100011"
     
     @staticmethod
     def _u_instruction(opcode: str, rd: str, imm: str) -> str:
-        return f"{int(imm):020b}{Linker.REGISTER_MAP[rd]}{opcode}"
+        return f"{int(imm):020b}{Assembler.REGISTER_MAP[rd]}{opcode}"
     
     @staticmethod
     def _lui_u_instruction(rd: str, imm: str) -> str:
-        return Linker._u_instruction("0110111", rd, imm)
+        return Assembler._u_instruction("0110111", rd, imm)
     
     @staticmethod
     def _auipc_u_instruction(rd: str, imm: str) -> str:
-        return Linker._u_instruction("0010111", rd, imm)
+        return Assembler._u_instruction("0010111", rd, imm)
     
     @staticmethod
     def _j_instruction(rd: str, imm: str) -> str:
         imm = f"{int(imm):020b}"
-        return f"{imm[0]}{imm[10]}{imm[9]}{imm[1:9]}{Linker.REGISTER_MAP[rd]}1101111"
+        return f"{imm[0]}{imm[10]}{imm[9]}{imm[1:9]}{Assembler.REGISTER_MAP[rd]}1101111"
 
-    def link(self, file_path: str) -> None:
+    def assemble(self, file_path: str) -> None:
         with open("out.bin", "bw") as out:
             pass
 
@@ -76,52 +76,52 @@ class Linker():
                     continue
                 
                 elif toks[0] == "addi":
-                    bin = Linker._arithmetic_i_instruction(toks[1], toks[2], toks[3], "000")
+                    bin = Assembler._arithmetic_i_instruction(toks[1], toks[2], toks[3], "000")
                 
                 elif toks[0] == "add":
-                    bin = Linker._r_instruction(toks[1], toks[2], toks[3], "000", "0000000")
+                    bin = Assembler._r_instruction(toks[1], toks[2], toks[3], "000", "0000000")
                 
                 elif toks[0] == "sub":
-                    bin = Linker._r_instruction(toks[1], toks[2], toks[3], "000", "0100000")
+                    bin = Assembler._r_instruction(toks[1], toks[2], toks[3], "000", "0100000")
                 
                 elif toks[0] == "slt":
-                    bin = Linker._r_instruction(toks[1], toks[2], toks[3], "010", "0000000")
+                    bin = Assembler._r_instruction(toks[1], toks[2], toks[3], "010", "0000000")
                 
                 elif toks[0] == "sltu":
-                    bin = Linker._r_instruction(toks[1], toks[2], toks[3], "011", "0000000")
+                    bin = Assembler._r_instruction(toks[1], toks[2], toks[3], "011", "0000000")
                 
                 elif toks[0] == "xor":
-                    bin = Linker._r_instruction(toks[1], toks[2], toks[3], "100", "0000000")
+                    bin = Assembler._r_instruction(toks[1], toks[2], toks[3], "100", "0000000")
                 
                 elif toks[0] == "or":
-                    bin = Linker._r_instruction(toks[1], toks[2], toks[3], "110", "0000000")
+                    bin = Assembler._r_instruction(toks[1], toks[2], toks[3], "110", "0000000")
                 
                 elif toks[0] == "lw":
                     imm, rest = strtol(toks[2])
                     rs1 = rest.replace("(", "").replace(")", "")
-                    bin = Linker._load_i_instruction(toks[1], rs1, imm, "010")
+                    bin = Assembler._load_i_instruction(toks[1], rs1, imm, "010")
                 
                 elif toks[0] == "sw":
                     imm, rest = strtol(toks[2])
                     rs1 = rest.replace("(", "").replace(")", "")
-                    bin = Linker._s_instruction(toks[1], rs1, imm, "010")
+                    bin = Assembler._s_instruction(toks[1], rs1, imm, "010")
                 
                 # M-extension
                 elif toks[0] == "mul":
-                    bin = Linker._r_instruction(toks[1], toks[2], toks[3], "000", "0000001")
+                    bin = Assembler._r_instruction(toks[1], toks[2], toks[3], "000", "0000001")
                 
                 elif toks[0] == "div":
-                    bin = Linker._r_instruction(toks[1], toks[2], toks[3], "100", "0000001")
+                    bin = Assembler._r_instruction(toks[1], toks[2], toks[3], "100", "0000001")
                 
                 # pseudo instructions
                 elif toks[0] == "li":
-                    bin = Linker._arithmetic_i_instruction(toks[1], "zero", toks[2], "000")
+                    bin = Assembler._arithmetic_i_instruction(toks[1], "zero", toks[2], "000")
                 
                 elif toks[0] == "seqz":
-                    bin = Linker._arithmetic_i_instruction(toks[1], "zero", "1", "011")
+                    bin = Assembler._arithmetic_i_instruction(toks[1], "zero", "1", "011")
                 
                 elif toks[0] == "snez":
-                    bin = Linker._r_instruction(toks[1], "zero", toks[2], "010", "0000000")
+                    bin = Assembler._r_instruction(toks[1], "zero", toks[2], "010", "0000000")
 
                 elif line == "ret":
                     continue
@@ -140,5 +140,5 @@ if __name__ == "__main__":
         print("The given number of arguments is invalid.", file=sys.stderr)
         sys.exit(1)
 
-    linker = Linker()
-    linker.link(args[1])
+    linker = Assembler()
+    linker.assemble(args[1])
